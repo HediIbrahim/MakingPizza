335ecb8011f565c3cbb7ab5cf4e8f7b8c98f4ff3
Hedi <HediIbrahim@users.noreply.github.com>
2017-04-15 23:52:32 -0400

    Add files via upload

---
+++ MakingPizza.playground/Contents.swift
@@ -0,0 +1,10 @@
+
+func makePizza() {
+print("Flattening the dough to form a round pizza 🖐")
+print("Adding some tomato sauce 🍅")
+print("Adding some mozzarella cheese 🧀")
+print("Adding some spicy pepper 🌶")
+print("Preparing the pizza in the oven ♨️")
+print("Done! Delicious pizza ready to be eaten 🍕")
+}
+makePizza()
